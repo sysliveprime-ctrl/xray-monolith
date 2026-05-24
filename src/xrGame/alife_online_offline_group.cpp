dc5ff2e165b5e2a9ac90875aaf105111afcd6e7b
sysliveprime-ctrl <sysliveprime-ctrl@users.noreply.github.com>
2026-05-24 08:39:45 +0300

    Restore offline group update behavior

--- src/xrGame/alife_online_offline_group.cpp
+++ src/xrGame/alife_online_offline_group.cpp
@@ -54,42 +54,28 @@ bool CSE_ALifeOnlineOfflineGroup::need_update(CSE_ALifeDynamicObject* object)
 
 void CSE_ALifeOnlineOfflineGroup::update()
 {
-    try
-    {
-        if (m_bOnline)
-        {
-            MEMBER* commander = (*m_members.begin()).second;
-            if (commander)
-            {
-                o_Position = commander->o_Position;
-                m_tNodeID = commander->m_tNodeID;
-                m_tGraphID = commander->m_tGraphID;
-            }
-        }
-        if (!bfActive())
-            return;
+	if (m_bOnline)
+	{
+		MEMBER* commander = (*m_members.begin()).second;
+		o_Position = commander->o_Position;
+		m_tNodeID = commander->m_tNodeID;
+		m_tGraphID = commander->m_tGraphID;
+	}
+	if (!bfActive())
+		return;
 
-        brain().update();
+	brain().update();
 
-        MEMBERS::iterator I = m_members.begin();
-        MEMBERS::iterator E = m_members.end();
-        for (; I != E; ++I)
-        {
-            MEMBER* ptr = ((*I).second);
-            if (ptr)
-            {
-                ptr->o_Position = o_Position;
-                ptr->m_tNodeID = m_tNodeID;
-                ptr->m_tGraphID = m_tGraphID;
-                ptr->m_fDistance = m_fDistance;
-            }
-
-        }
-    }
-    catch (...)
-    {
-
-    }
+	MEMBERS::iterator I = m_members.begin();
+	MEMBERS::iterator E = m_members.end();
+	for (; I != E; ++I)
+	{
+		((*I).second)->o_Position = o_Position;
+		((*I).second)->m_tNodeID = m_tNodeID;
+		((*I).second)->m_tGraphID = m_tGraphID;
+		((*I).second)->m_fDistance = m_fDistance;
+	}
+	return;
 }
 
 void CSE_ALifeOnlineOfflineGroup::on_location_change() const
@@ -184,13 +170,10 @@ bool CSE_ALifeOnlineOfflineGroup::synchronize_location()
 	if (m_bOnline)
 	{
 		MEMBER* member = (*m_members.begin()).second;
-        if (member)
-        {
-            o_Position = member->o_Position;
-            m_tNodeID = member->m_tNodeID;
-            m_tGraphID = member->m_tGraphID;
-            m_fDistance = member->m_fDistance;
-        }
+		o_Position = member->o_Position;
+		m_tNodeID = member->m_tNodeID;
+		m_tGraphID = member->m_tGraphID;
+		m_fDistance = member->m_fDistance;
 	}
 
 	return (true);
